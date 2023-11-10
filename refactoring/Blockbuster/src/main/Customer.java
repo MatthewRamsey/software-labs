f34c9f91b7122467d676b4dcc94f42ec313d5d1b
Matthew Ramsey <ramsey.matthew@live.com>
2023-11-10 07:43:25 -0600

    move method to other class

--- refactoring/Blockbuster/src/main/Customer.java
+++ refactoring/Blockbuster/src/main/Customer.java
@@ -29,9 +29,6 @@ public class Customer {
                 double thisAmount = 0;
                 Rental each = (Rental) rentals.nextElement();
 
-                //determine amounts for each line
-                thisAmount = amountFor(each);
-
                 //add frequent renter points
                 frequentRenterPoints++;
                 //add bonus for a two day new release rental
@@ -39,8 +36,8 @@ public class Customer {
                     frequentRenterPoints++;
 
                 //show figures for this rental
-                result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-                totalAmount += thisAmount;
+                result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+                totalAmount += each.getCharge();
             }
 
             //add footer lines
@@ -48,24 +45,4 @@ public class Customer {
             result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
             return result;
         }
-
-    private static double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch(each.getMovie().getPriceCode()){
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if(each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if(each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
 }
